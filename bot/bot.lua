76f3ca7db579bd68e94965e95c179b439d4caf24
Rastin <61466696+ibornco@users.noreply.github.com>
2020-03-01 18:11:59 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,3 @@
--- #Beyond Reborn Robot
--- #@BeyondTeam
 
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -90,28 +88,18 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {866766265, 0, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
     info_text = [[》Beyond Reborn v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://github.com/ibornco/db
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》@iBorn ➣ Founder & Developer《
 
-》Special thanks to :
-》Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://Beyond-Dev.iR
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
